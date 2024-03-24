d23c31396399fa9a102b8a22db81e0e06f8f9dd2
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-03-24 13:33:54 +0200

    chore: add complex views

---
+++ example/src/NestedView.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { StyleSheet, View, Text } from 'react-native';
+
+interface NestedViewProps {
+  children?: React.ReactNode;
+  depth: number;
+  breadth?: number;
+}
+
+export const NestedView: React.FC<NestedViewProps> = ({
+  depth,
+  breadth = 1,
+  children,
+}) => {
+  if (!depth) {
+    return <>{children}</>;
+  }
+  return (
+    <View style={styles.container}>
+      <Text>{depth}</Text>
+      {new Array(breadth).fill(null).map((_, index) => (
+        <NestedView key={index} breadth={breadth} depth={depth - 1} />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    padding: 1,
+  },
+});
